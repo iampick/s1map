effa92d95b3677dc9f9b64953dbf1cd8ebca3051
iampick <sir.pick@gmail.com>
2023-02-19 13:56:25 +0700

    map2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 
 const containerStyle = {
   width: '100%',
-  height: '96vh',
+  height: '90vh',
 };
 
 const center = {
@@ -25,10 +25,10 @@ const handleKmlError = (error) => {
 };
 
 function MyComponent() {
-  const [markers, setMarkers] = useState([]);
-const [kmlData, setKmlData] = useState(
-  'http://45.76.146.132:3100/Hydranty.kml',
-);
+  const [markers, setMarkers] = useState([
+    { lat: 41.851587179281076, lng: -87.62039511108398 },
+  ]);
+  const [kmlData, setKmlData] = useState(null);
 
   useEffect(() => {
     const savedMarkers = JSON.parse(localStorage.getItem('markers'));
@@ -51,27 +51,41 @@ const [kmlData, setKmlData] = useState(
     localStorage.setItem('markers', JSON.stringify(markers));
   }, [markers]);
 
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setKmlData('http://45.76.146.132:3100/s1kml.kml');
+  //   }, 3000);
+  // }, []);
+
+  const handleKml = () => {
+    setKmlData('http://45.76.146.132:3100/s1kml.kml');
+    console.log('KML Click');
+  };
   return (
-    <LoadScript googleMapsApiKey="AIzaSyCHTBblEvzCzdWu2fIIqNjFbbuMhqKGP2k">
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={3}
-        onClick={handleMapClick}
-      >
-        {/* Child components, such as markers, info windows, etc. */}
-        {markers.map((marker, index) => (
-          <Marker key={index} position={marker} onLoad={onLoad} />
-        ))}
-        <KmlLayer
-          url="http://45.76.146.132:3100/Hydranty.kml"
-          // url="https://www.strongmotioncenter.org/map/cosmosVDC.kml"
-          onUnmount={onLoad}
-          onLoad={onLoad}
-          onError={handleKmlError}
-          preserveViewport={true}
-        />
-        {/* <KmlLayer
+    <>
+      <LoadScript googleMapsApiKey="AIzaSyCHTBblEvzCzdWu2fIIqNjFbbuMhqKGP2k">
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={3}
+          onClick={handleMapClick}
+        >
+          {/* Child components, such as markers, info windows, etc. */}
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker} onLoad={onLoad} />
+          ))}
+          {kmlData && (
+            <KmlLayer
+              url={kmlData}
+              // url="https://www.strongmotioncenter.org/map/cosmosVDC.kml"
+              onUnmount={onLoad}
+              onLoad={onLoad}
+              onError={handleKmlError}
+              preserveViewport={true}
+            />
+          )}
+
+          {/* <KmlLayer
           // url="http://45.76.146.132:3100/s1kml.kml"
           url="https://www.strongmotioncenter.org/map/cosmosVDC.kml"
           onUnmount={onLoad}
@@ -79,9 +93,13 @@ const [kmlData, setKmlData] = useState(
           onError={handleKmlError}
           preserveViewport={true}
         /> */}
-        {kmlData && <KmlLayer url={kmlData} />}
-      </GoogleMap>
-    </LoadScript>
+          {/* {kmlData && <KmlLayer url={kmlData} />} */}
+        </GoogleMap>
+      </LoadScript>
+      <div className="row">
+        <button onClick={handleKml}>Show kml</button>
+      </div>
+    </>
   );
 }
 
